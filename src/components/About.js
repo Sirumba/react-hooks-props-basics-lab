99d9112c9dbe9e15b5eb82f1cba3294b54bce3c3
Sirumba <simonsmaina45@gmail.com>
2023-01-15 18:25:52 +0300

    one more to go

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,20 @@
 import React from "react";
+import Links from "./Links"
 
-function About() {
+function About({bio}) {
+  if(!bio || ""){
+    return null;
+  } else {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <p>{bio}</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github="https://github.com/liza" linkedin={"https://www.linkedin.com/in/liza/"}/>
+     
     </div>
   );
+  }
 }
 
 export default About;
